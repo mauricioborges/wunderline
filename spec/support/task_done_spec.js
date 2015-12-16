f1b46edbb459af5c13329847dbef0228bdae5acc
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-16 16:51:38 -0200

    wip spec

---
+++ spec/support/task_done_spec.js
@@ -0,0 +1,7 @@
+describe("Task status manipulation" ,function() {
+    describe("Not completed task", function() {
+        it("send a PATCH request to /api/v1/tasks",hf function() {
+
+
+    });
+});
